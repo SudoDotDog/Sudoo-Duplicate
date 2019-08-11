6b065823460f4c565a5555eeabbc4a80417facf1
WMXPY <wm@mengw.io>
2019-08-11 01:42:30 -0500

    implement duplicate function

---
+++ src/duplicate.ts
@@ -0,0 +1,52 @@
+/**
+ * @author WMXPY
+ * @namespace Duplicate
+ * @description Duplicate
+ */
+
+import { types } from "util";
+
+export const duplicateObject = <T extends any>(target: T): T => {
+
+    if (target === null) {
+        return target;
+    }
+
+    if (Array.isArray(target)) {
+
+        const asserted: any[] = target as any[];
+        return asserted.map((each: any) => duplicateObject(each)) as any as T;
+    }
+
+    if (typeof target === 'object') {
+
+        const asserted: Record<any, any> = target as any;
+        return Object.entries(asserted).reduce((previous: Record<any, any>, current: [any, any]) => {
+            return {
+                ...previous,
+                [current[0]]: current[1],
+            };
+        }, {} as Record<any, any>) as any as T;
+    }
+
+    if (typeof target === 'function') {
+
+        // tslint:disable-next-line: ban-types
+        const asserted: Function = target as Function;
+        return function () {
+            // tslint:disable-next-line: no-invalid-this
+            return asserted.apply(this, arguments);
+        } as any as T;
+    }
+
+    if (types.isDate(target)) {
+
+        const asserted: Date = target as Date;
+        const clone: Date = new Date();
+        clone.setTime(asserted.getTime());
+
+        return clone as any as T;
+    }
+
+    return target;
+};
