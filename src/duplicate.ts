731c432d66b6d51bb4b48bbf2368d1d07270c2cb
WMXPY <wm@mengw.io>
2019-11-16 15:45:00 -0600

    implemnt better clone function

--- src/duplicate.ts
+++ src/duplicate.ts
@@ -42,19 +42,50 @@ export const duplicate = <T extends any>(target: T): T => {
 
     if (isObject(target)) {
 
-        try {
-            const clone: T = new (target as any).constructor();
-            for (const entry of Object.entries(target)) {
-                (clone as any)[entry[0]] = duplicate(entry[1]);
-            }
-
-            return clone;
-        } catch (error) {
-
-            const clone = Object.assign(Object.create(Object.getPrototypeOf(target)), target);
-            return clone;
+        const constructorCloned: T | null = constructorCloneObject(target);
+        if (constructorCloned) {
+            return constructorCloned;
         }
+        const prototypeCloned: T | null = prototypeCloneObject(target);
+        if (prototypeCloned) {
+            return prototypeCloned;
+        }
+
+        return target;
     }
 
     return target;
+};
+
+export const constructorCloneObject = <T extends any = any>(target: T): T | null => {
+
+    try {
+
+        const clone: T = new (target as any).constructor();
+
+        for (const entry of Object.entries(target)) {
+            (clone as any)[entry[0]] = duplicate(entry[1]);
+        }
+        return clone;
+    } catch (error) {
+
+        return null;
+    }
+};
+
+export const prototypeCloneObject = <T extends any = any>(target: T): T | null => {
+
+    try {
+
+        const prototype: any = Object.getPrototypeOf(target);
+        const clone: any = Object.create(prototype);
+
+        for (const entry of Object.entries(target)) {
+            (clone as any)[entry[0]] = duplicate(entry[1]);
+        }
+        return clone;
+    } catch (error) {
+
+        return null;
+    }
 };
