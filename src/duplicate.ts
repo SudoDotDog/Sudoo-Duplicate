5b73d360fb603183de0c7e6da57f3f6807fff879
WMXPY <wm@mengw.io>
2020-05-28 12:33:36 -0500

    release 1.3.0, fix type issue

--- src/duplicate.ts
+++ src/duplicate.ts
@@ -6,41 +6,49 @@
 
 import { isArray, isBigint, isDate, isFunction, isMap, isNull, isObject, isRegExp, isSet } from "./util";
 
+
+const _constructorCloneObject = <T extends any>(
+    target: T,
+    _duplicate: (target: T, parent?: any) => any,
+): T | null => {
+    try {
+        const clone: T = new (target as any).constructor();
+        for (const entry of Object.entries(target as any)) {
+            (clone as any)[entry[0]] = _duplicate(entry[1] as any, clone);
+        }
+        return clone;
+    } catch (error) {
+        return null;
+    }
+};
+
+const _prototypeCloneObject = <T extends any>(
+    target: T,
+    _duplicate: (target: T, parent?: any) => any,
+): T | null => {
+    try {
+        const prototype: any = Object.getPrototypeOf(target);
+        const clone: any = Object.create(prototype);
+        for (const entry of Object.entries(target as any)) {
+            (clone as any)[entry[0]] = _duplicate(entry[1] as any, clone);
+        }
+        return clone;
+    } catch (error) {
+        return null;
+    }
+};
+
 export const duplicate = <T extends any>(target: T): T => {
 
     const recursiveWatcher: Set<any> = new Set();
 
-    const _duplicate = (innerTarget: T, parent?: any) => {
+
+    const _duplicate = (innerTarget: T, parent?: any): T => {
 
         if (recursiveWatcher.has(innerTarget)) {
             return innerTarget;
         }
         recursiveWatcher.add(innerTarget);
-
-        const _constructorCloneObject = (constructorCloneTarget: T): T | null => {
-            try {
-                const clone: T = new (constructorCloneTarget as any).constructor();
-                for (const entry of Object.entries(constructorCloneTarget)) {
-                    (clone as any)[entry[0]] = _duplicate(entry[1], clone);
-                }
-                return clone;
-            } catch (error) {
-                return null;
-            }
-        };
-
-        const _prototypeCloneObject = (prototypeCloneTarget: T): T | null => {
-            try {
-                const prototype: any = Object.getPrototypeOf(prototypeCloneTarget);
-                const clone: any = Object.create(prototype);
-                for (const entry of Object.entries(prototypeCloneTarget)) {
-                    (clone as any)[entry[0]] = _duplicate(entry[1], clone);
-                }
-                return clone;
-            } catch (error) {
-                return null;
-            }
-        };
 
         if (isNull(innerTarget)) {
             return innerTarget;
@@ -86,11 +94,11 @@ export const duplicate = <T extends any>(target: T): T => {
 
         if (isObject(innerTarget)) {
 
-            const constructorCloned: T | null = _constructorCloneObject(innerTarget);
+            const constructorCloned: T | null = _constructorCloneObject(innerTarget, _duplicate);
             if (constructorCloned) {
                 return constructorCloned;
             }
-            const prototypeCloned: T | null = _prototypeCloneObject(innerTarget);
+            const prototypeCloned: T | null = _prototypeCloneObject(innerTarget, _duplicate);
             if (prototypeCloned) {
                 return prototypeCloned;
             }
